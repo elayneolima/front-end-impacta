30d5cd7ceed201cdded88d78854c3b5863664a64
elayneolima <elayne04lima@gmail.com>
2025-11-24 18:35:20 -0300

    Integrando com API

--- js/validate.form.js
+++ js/validate.form.js
@@ -4,6 +4,58 @@
         const nome = doc.getElementById('recipient-name');
         const mensagem = doc.getElementById('message-text')
         const avisoResposta = doc.querySelector('[data-resposta="mensagem"]')
+        const cep = doc.getElementById('recipient-cep');
+        const logradouro = doc.getElementById('recipient-logradouro');
+        
+        
+        
+        //poderia ser focus
+        nome.addEventListener('blur', ()=>{
+          const cepMemory =  localStorage.getItem('dados-form')
+          const convertCepMemory = JSON.parse(cepMemory)
+          logradouro.value = cepMemory.logradouro
+          cep.value = cepMemory.cep
+
+        })
+        
+        cep.addEventListener('keyup', (event) => {
+            if(event.key === 'Enter'){
+              buscaPorCep(event.target.value)
+              console.log('chamada da api')
+            }
+           
+        
+         
+        })
+
+        //async + await
+        async function buscaPorCep(cep){
+          const cepMemory =  localStorage.getItem('dados-form')
+
+          if(!cepMemory){
+             const busca = await fetch(`https://viacep.com.br/ws/${cep}/json/`) // fetch espera uma str e retorna uma promisse (obj js que contem os dados que voltam do server)
+             const dados = await busca.json()
+
+             logradouro.value = dados.logradouro
+             const json = JSON.stringify({cep: cep.value, logradouro: dados.logradouro})
+             return; // se ela foi chamada uma vez, significa ja armazenou e nao precisa entrar mais
+          }
+
+          const convertCepMemory = JSON.parse(cepMemory)
+          logradouro.value = cepMemory.logradouro
+          cep.value = cepMemory.cep
+
+         
+      
+
+                    //chave / valor
+          localStorage.setItem('dados-form', json)
+          //sesionStorage é para que a informação seja deletada quando a aba for deletada, nao guarda na sessão
+          
+          // conosle.log(cep)
+          //await é um aguardar
+        }
+
 
         btn.addEventListener('click', (event) => {
             event.preventDefault();
@@ -40,7 +92,7 @@
 
 
          //FORMATO JSON deve ser uma string ``, as propriedades precisam estar entre asas
-         const objeto = {nome : nome, mensagem: mensagem }
+         const objeto = {nome: nome, mensagem: mensagem}
          const json = JSON.stringify(objeto)//converte um objeto js em string json 
          
          //aguarda uma resposta do servidor
@@ -50,7 +102,11 @@
             //converte a string que chega em um opbjeto
             const dados = JSON.parse(xhr.responseText); 
             avisoResposta.innerHTML = dados.status;
+            nome.value = ''
+            mensagem.value = ''
             console.log(dados.status)// é onde fica guardada a informação quevem da response
+            cep.value = ''
+            logradouro.value = ''
           }
           else if (xhr.readyState === 4 && xhr.status === 400){
              console.log(xhr.responseText)
@@ -62,8 +118,36 @@
          //JSON É UM FORMATO DE ARQUIVOI PARA ENVIAR INFO PRO SERVIDOR PRO CLIENTE ETC. 
           xhr.send(json)
         }
+
+
+
+
+
+
+
+
+
+
+
         // const resp = {status: 'Dados cadastrados'}
         // console.log(resp.status)
+        // let Pessoa = function(nome){
+        //   this.nome = nome;
+        // }
+
+        //essa função de callback nao é executando dentro do objeto e sim em outro lugar
+        //por este motivo ele se perde
+        // Pessoa.prototype.getNome = function(){
+            // let nome = this.nome;
+        //     ['ana', 'pedro'].forEach(function(elemento){
+        //       if (elemento == this.nome){
+        //         console.log('Achei!')
+        //       }
+        //     }.bind(this)) // quando mandamos o this, ele reconehce na funcao
+        // }
+
+        // let pessoa = new Pessoa('ana')
+        // pessoa.getNome()
 
   })(document)
 
